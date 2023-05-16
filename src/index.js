9abeda22976470b21c5483108cf7d9a24817636a
Junaid Ahmed Syed <61676854+iamsjunaid@users.noreply.github.com>
2023-05-16 23:07:19 +0530

    add html wireframe

--- src/index.js
+++ src/index.js
@@ -1,2 +1,1 @@
-import "./index.css";
-console.log('Hello, webpack!');
+import './index.css';
--- webpack.config.js
+++ webpack.config.js
@@ -2,31 +2,30 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    devServer: {
-        static: './dist',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Output Management',
-            template: './src/index.html',
-        }),
+  entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    optimization: {
-        runtimeChunk: 'single',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-        ],
-    },
-    mode: 'development',
+  },
+  mode: 'none',
 };
